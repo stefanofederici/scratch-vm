5b44fa83094c19f27f36bee4f4c53bb2814f00ed
Thomas Weber <weber_t@mailbox.org>
2020-10-06 14:05:44 -0500

    Use requestAnimationFrame instead of setInterval when possible

--- src/engine/tw-frame-loop.js
+++ src/engine/tw-frame-loop.js
@@ -4,22 +4,54 @@ class FrameLoop {
         this.stepTime = 1000;
         this.callback = () => {};
         this._interval = null;
+        this._nextAnimationFrame = null;
+        this.handleAnimationFrame = this.handleAnimationFrame.bind(this);
+        this.lastFrameTime = null;
+        this.timeToNextFrame = null;
     }
 
     isRunning () {
         return this.running;
     }
 
+    handleAnimationFrame (time) {
+        this._nextAnimationFrame = requestAnimationFrame(this.handleAnimationFrame);
+
+        if (this.lastFrameTime === null) {
+            this.lastFrameTime = time;
+        }
+
+        const deltaTime = time - this.lastFrameTime;
+        this.lastFrameTime = time;
+
+        this.timeToNextFrame -= deltaTime;
+
+        if (this.timeToNextFrame <= 0) {
+            this.timeToNextFrame += this.stepTime;
+            this.callback();
+        }
+    }
+
     start (callback, stepTime) {
         this.running = true;
         this.stepTime = stepTime;
         this.callback = callback;
-        this._interval = setInterval(this.callback, stepTime);
+        // requestAnimationFrame is not available in Node
+        if (typeof requestAnimationFrame === 'undefined') {
+            this._interval = setInterval(callback, stepTime);
+        } else {
+            this.lastFrameTime = null;
+            this.timeToNextFrame = 0;
+            this._nextAnimationFrame = requestAnimationFrame(this.handleAnimationFrame);
+        }
     }
 
     stop () {
         this.running = false;
         clearInterval(this._interval);
+        if (this._nextAnimationFrame) {
+            cancelAnimationFrame(this._nextAnimationFrame);
+        }
     }
 }
 
