c8d30d06205914b870bfb375f7b2464b9fc0a34f
Thomas Weber <weber_t@mailbox.org>
2020-12-22 17:58:54 -0600

    Some work on making interpolation framerate independent

--- src/engine/interpolate.js
+++ src/engine/interpolate.js
@@ -30,7 +30,12 @@ const setupTargets = runtime => {
     }
 };
 
-const interpolateTargets = runtime => {
+/**
+ * Interpolate the position of targets.
+ * @param {Runtime} runtime The Runtime with targets to interpolate
+ * @param {number} time Relative mid-frame time. [0-1]
+ */
+const interpolateTargets = (runtime, time) => {
     const renderer = runtime.renderer;
     if (!renderer) {
         return;
@@ -45,9 +50,11 @@ const interpolateTargets = runtime => {
         const drawableID = target.drawableID;
 
         // Position interpolation.
-        const xDistance = Math.abs(target.x - interpolationData.x);
-        const yDistance = Math.abs(target.y - interpolationData.y);
-        if (xDistance > 0.1 || yDistance > 0.1) {
+        const xDistance = target.x - interpolationData.x;
+        const yDistance = target.y - interpolationData.y;
+        const absoluteXDistance = Math.abs(xDistance);
+        const absoluteYDistance = Math.abs(yDistance);
+        if (absoluteXDistance > 0.1 || absoluteYDistance > 0.1) {
             const drawable = renderer._allDrawables[drawableID];
             // getAABB is less accurate than getBounds, but it's much faster
             const bounds = drawable.getAABB();
@@ -55,18 +62,19 @@ const interpolateTargets = runtime => {
             const xTolerance = Math.min(50, 10 + bounds.width);
             const yTolerance = Math.min(50, 10 + bounds.height);
 
-            if (xDistance < xTolerance && yDistance < yTolerance) {
-                const newX = (interpolationData.x + target.x) / 2;
-                const newY = (interpolationData.y + target.y) / 2;
+            if (absoluteXDistance < xTolerance && absoluteYDistance < yTolerance) {
+                const newX = interpolationData.x + xDistance * time;
+                const newY = interpolationData.y + yDistance * time;
                 renderer.updateDrawablePosition(drawableID, [newX, newY]);
             }
         }
 
         // Effect interpolation.
-        const ghostChange = Math.abs(target.effects.ghost - interpolationData.ghost);
-        // Make sure we don't interpolate a change from 0 to 100 ghost or other large changes.
-        if (ghostChange > 0 && ghostChange < 25) {
-            const newGhost = (target.effects.ghost + interpolationData.ghost) / 2;
+        const ghostChange = target.effects.ghost - interpolationData.ghost;
+        const absoluteGhostChange = Math.abs(ghostChange);
+        // Do not interpolate large effect changes. Anything large is probably intended to be instantaneous.
+        if (absoluteGhostChange > 0 && absoluteGhostChange < 25) {
+            const newGhost = target.effects.ghost + ghostChange * time;
             renderer.updateDrawableEffect(drawableID, 'ghost', newGhost);
         }
 
@@ -78,10 +86,12 @@ const interpolateTargets = runtime => {
             let scale = targetDirectionAndScale.scale;
             let updateDrawableDirectionScale = false;
 
+            // Interpolate direction.
             if (direction !== interpolationData.direction) {
                 // The easiest way to find the average of two angles is using trig functions.
                 const currentRadians = direction * Math.PI / 180;
                 const startingRadians = interpolationData.direction * Math.PI / 180;
+                // TODO: interpolate using time
                 direction = Math.atan2(
                     Math.sin(currentRadians) + Math.sin(startingRadians),
                     Math.cos(currentRadians) + Math.cos(startingRadians)
@@ -90,16 +100,20 @@ const interpolateTargets = runtime => {
                 updateDrawableDirectionScale = true;
             }
 
+            // Interpolate scale.
             const startingScale = interpolationData.scale;
             if (scale[0] !== startingScale[0] || scale[1] !== startingScale[1]) {
                 // Do not interpolate size when the sign of either scale differs.
                 if (Math.sign(scale[0]) === Math.sign(startingScale[0]) && Math.sign(scale[1]) === Math.sign(startingScale[1])) {
-                    const change = Math.abs(scale[0] - startingScale[0]);
+                    const changeX = scale[0] - startingScale[0];
+                    const changeY = scale[1] - startingScale[1];
+                    const absoluteChangeX = Math.abs(changeX);
+                    const absoluteChangeY = Math.abs(changeY);
                     // Only interpolate on small enough sizes. Anything larger is likely intended to be an instant change.
-                    if (change < 100) {
+                    if (absoluteChangeX < 100 && absoluteChangeY < 100) {
                         scale = [
-                            (scale[0] + startingScale[0]) / 2,
-                            (scale[1] + startingScale[1]) / 2
+                            startingScale[0] + changeX * time,
+                            startingScale[1] + changeY * time
                         ];
                         updateDrawableDirectionScale = true;
                     }
