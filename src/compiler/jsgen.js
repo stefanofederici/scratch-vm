2db141f0188cab3c90b3df43ff0d230750062fa9
Thomas Weber <weber_t@mailbox.org>
2021-03-22 14:53:00 -0500

    pointerlock: js: Fix type of mouse.x and mouse.y

--- src/compiler/jsgen.js
+++ src/compiler/jsgen.js
@@ -455,9 +455,9 @@ class JSGenerator {
         case 'mouse.down':
             return new TypedInput('runtime.ioDevices.mouse.getIsDown()', TYPE_BOOLEAN);
         case 'mouse.x':
-            return new TypedInput('runtime.ioDevices.mouse.getScratchX()', TYPE_NUMBER);
+            return new TypedInput('runtime.ioDevices.mouse.getScratchX()', TYPE_UNKNOWN);
         case 'mouse.y':
-            return new TypedInput('runtime.ioDevices.mouse.getScratchY()', TYPE_NUMBER);
+            return new TypedInput('runtime.ioDevices.mouse.getScratchY()', TYPE_UNKNOWN);
 
         case 'op.abs':
             return new TypedInput(`Math.abs(${this.descendInput(node.value).asNumber()})`, TYPE_NUMBER);
