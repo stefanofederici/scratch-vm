f6771c4895c4504a9df5bc36ad50bc03fb358df6
Thomas Weber <weber_t@mailbox.org>
2021-05-22 09:30:18 -0500

    Load extension worker from blob

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -171,7 +171,7 @@ class ExtensionManager {
         return new Promise((resolve, reject) => {
             // If we `require` this at the global level it breaks non-webpack targets, including tests
             // eslint-disable-next-line max-len
-            const ExtensionWorker = require('worker-loader?name=js/extension-worker/extension-worker.[hash].js!./extension-worker');
+            const ExtensionWorker = require('worker-loader!./extension-worker');
 
             this.pendingExtensions.push({extensionURL, resolve, reject});
             dispatch.addWorker(new ExtensionWorker());
