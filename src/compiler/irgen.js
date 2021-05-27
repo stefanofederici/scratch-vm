5f34ee56433fec85eb2d54547f57edab006de7db
Thomas Weber <weber_t@mailbox.org>
2021-05-27 18:07:58 -0500

    is packaged?

--- src/compiler/irgen.js
+++ src/compiler/irgen.js
@@ -207,7 +207,7 @@ class ScriptTreeGenerator {
             const name = block.fields.VALUE.value;
             const index = this.script.arguments.lastIndexOf(name);
             if (index === -1) {
-                if (name.toLowerCase() === 'is compiled?' || name.toLowerCase() === 'is turbowarp?') {
+                if (name.toLowerCase() === 'is compiled?' || name.toLowerCase() === 'is turbowarp?' || name.toLowerCase() === 'is packaged?') {
                     return {
                         kind: 'constant',
                         value: true
